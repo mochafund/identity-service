8b8f998e3d52c9de5041fd4c3542995a9a43ff48
Connor Burge <connor.d.burge@gmail.com>
2025-09-17 21:22:17 -0400

    @Builder.Default in BaseEvent

--- src/main/java/com/mochafund/identityservice/common/events/BaseEvent.java
+++ src/main/java/com/mochafund/identityservice/common/events/BaseEvent.java
@@ -1,13 +1,15 @@
 package com.mochafund.identityservice.common.events;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -17,9 +19,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
 public abstract class BaseEvent {
+    @Builder.Default
     private UUID id = UUID.randomUUID();
+    @Builder.Default
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Instant publishedAt = Instant.now();
     private UUID correlationId;
-    private LocalDateTime publishedAt = LocalDateTime.now();
     private String type;
     private String actor;
 }
